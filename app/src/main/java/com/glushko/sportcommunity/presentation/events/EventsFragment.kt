61f62f5bfba100f0d2e90e7db71fa791ca1b7f43
Mark Glushko <m.glushko@wnfx.ru>
2022-08-17 01:26:17 +0300

    Добавил новые вкладки

---
+++ app/src/main/java/com/glushko/sportcommunity/presentation/events/EventsFragment.kt
@@ -0,0 +1,7 @@
+package com.glushko.sportcommunity.presentation.events
+
+import androidx.fragment.app.Fragment
+import com.glushko.sportcommunity.R
+
+class EventsFragment: Fragment(R.layout.fragment_events) {
+}
