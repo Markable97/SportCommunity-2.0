a19f0f2c7206ef98d44d88ca78bcdd8d4163df69
Mark Glushko <m.glushko@wnfx.ru>
2022-11-02 00:28:00 +0300

    fixed designed + added floating button + navigate to create schedule fragment

--- app/src/main/java/com/glushko/sportcommunity/presentation/admin/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/glushko/sportcommunity/presentation/admin/schedule/ScheduleFragment.kt
@@ -14,6 +14,7 @@ import com.glushko.sportcommunity.presentation.admin.schedule.adapters.ScheduleA
 import com.glushko.sportcommunity.presentation.base.BaseXmlFragment
 import com.glushko.sportcommunity.util.Result
 import com.glushko.sportcommunity.util.data
+import com.glushko.sportcommunity.util.extensions.setSafeOnClickListener
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
@@ -74,6 +75,13 @@ class ScheduleFragment : BaseXmlFragment<FragmentScheduleBinding>(R.layout.fragm
         super.onViewCreated(view, savedInstanceState)
         initRecycler()
         setupObservers()
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.floatingButtonCreateSchedule.setSafeOnClickListener {
+            findNavController().navigate(ScheduleFragmentDirections.actionScheduleFragmentToCreateScheduleFragment())
+        }
     }
 
     private fun setupObservers() {
