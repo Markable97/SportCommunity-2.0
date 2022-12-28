274ee17eea355b63c30812e251b7622a4d225b80
Mark Glushko <m.glushko@wnfx.ru>
2022-12-28 21:40:32 +0300

    added download image

--- app/src/main/java/com/glushko/sportcommunity/presentation/media/GalleryFragment.kt
+++ app/src/main/java/com/glushko/sportcommunity/presentation/media/GalleryFragment.kt
@@ -1,26 +1,34 @@
 package com.glushko.sportcommunity.presentation.media
 
+import android.app.DownloadManager
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.platform.ComposeView
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.getSystemService
 import com.glushko.sportcommunity.R
 import com.glushko.sportcommunity.data.media.model.ImageUI
 import com.glushko.sportcommunity.presentation.base.BaseFragment
 import com.glushko.sportcommunity.presentation.core.DoSomething
 import com.glushko.sportcommunity.presentation.core.Loader
 import com.glushko.sportcommunity.util.Result
-import com.glushko.sportcommunity.util.extensions.toast
 import com.glushko.sportcommunity.util.extensions.toastLong
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
+import java.io.File
+
 
 @AndroidEntryPoint
 class GalleryFragment: BaseFragment() {
@@ -77,7 +85,29 @@ class GalleryFragment: BaseFragment() {
                 GalleryScreen(
                     imagesList = (imagesResponse as Result.Success<List<ImageUI>>).data,
                     onClickDownload = { url ->
+                        try {
+                            val fileName = "${System.currentTimeMillis()}"
+                            val dm = requireActivity().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
 
+                            val downloadUri: Uri = Uri.parse(url)
+                            val request = DownloadManager.Request(downloadUri)
+                            request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+                                .setAllowedOverRoaming(false)
+                                .setTitle(fileName)
+                                .setMimeType("image/jpeg") // Your file type. You can use this code to download other file types also.
+                                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                                .setDestinationInExternalPublicDir(
+                                    Environment.DIRECTORY_PICTURES,
+                                    File.separator + fileName + ".jpg"
+                                )
+                            dm.enqueue(request)
+                            Toast.makeText(requireContext(), R.string.download_start, Toast.LENGTH_SHORT)
+                                .show()
+                        } catch (e: Exception) {
+                            Timber.e(e)
+                            Toast.makeText(requireContext(), R.string.download_error, Toast.LENGTH_SHORT)
+                                .show()
+                        }
                     },
                     onClickShare = { bitmap ->
                         mainViewModel.getUriToShare(bitmap, requireActivity().cacheDir.toString(), requireContext())
