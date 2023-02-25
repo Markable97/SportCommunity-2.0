109b3425c84b2fe43022e53a7c8069984458f771
Mark Glushko <m.glushko@wnfx.ru>
2023-02-25 13:45:50 +0300

    changed logic getting version mane

--- app/src/main/java/com/glushko/sportcommunity/presentation/about/AboutFragment.kt
+++ app/src/main/java/com/glushko/sportcommunity/presentation/about/AboutFragment.kt
@@ -1,11 +1,10 @@
 package com.glushko.sportcommunity.presentation.about
 
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.glushko.sportcommunity.BuildConfig
 import com.glushko.sportcommunity.R
 
 class AboutFragment: Fragment(R.layout.fragment_about_us) {
@@ -13,10 +12,7 @@ class AboutFragment: Fragment(R.layout.fragment_about_us) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<TextView>(R.id.textVersionBuild).apply {
-            val versionName = requireContext().packageManager.getPackageInfo(
-                requireActivity().packageName,
-                PackageManager.PackageInfoFlags.of(0L)).versionName
-            text = getString(R.string.app_version, versionName)
+            text = getString(R.string.app_version, BuildConfig.VERSION_NAME)
         }
     }
 
